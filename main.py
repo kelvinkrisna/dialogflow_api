1c9ccd3694be4ced7c73587fd376a539b1564d6a
Kelvin Silva <kelvin.silva@gmail.com>
2024-11-19 00:19:08 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from random_address import real_random_address
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def studentNumber():
+    return {'studentnumber': '200585799'}
+
+@app.route('/webhook', methods=['GET'])
+def bagposition():
+    customaddress = real_random_address()
+    return customaddress
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=105)

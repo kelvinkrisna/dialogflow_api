0e0123535d7147124cc724bc99f4afd8bdcd33ec
Kelvin Silva <kelvin.silva@gmail.com>
2024-11-21 01:39:55 -0500

    committed

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def studentNumber():
     return {'studentnumber': '200585799'}
 
-@app.route('/webhook', methods=['GET'])
+@app.route('/webhook', methods=['GET', 'POST'])
 def bagposition():
-    customaddress = real_random_address(
+    customaddress = real_random_address()
+    return customaddress
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=105)
